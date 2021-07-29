5d125478893672edebee55bd3cf0c7713e51db70
AhmedSafwat1 <Ahmed.Safwat1235@gmail.com>
2021-07-29 17:42:40 +0200

    finsh

--- server.js
+++ server.js
@@ -13,11 +13,11 @@
     const app = express();
 
     // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/demo-deploydssada'));
+    app.use(express.static(__dirname + '/dist/demo-deploy'));
 
     app.get('/*', function(req,res) {
         
-    res.sendFile(path.join(__dirname+'/dist/demo-deploydssada/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
     });
 
     // Start the app by listening on the default Heroku port
